c77be0694af3aff80158ccaccf6ee8246f79bdd1
Sơn <126846925+Congson2401@users.noreply.github.com>
2025-05-25 15:46:08 +0700

    Add files via upload

---
+++ scr/main/java/common/Constant.java
@@ -0,0 +1,10 @@
+package common;
+import org.openqa.selenium.WebDriver;
+    public class Constant {
+        public static WebDriver WEBDRIVER;
+        public static final String RAILWAY_URL="http://railwayb1.somee.com/Page/HomePage.cshtml";
+        public static final String RAILWAY_LOGIN_URL = "http://railwayb1.somee.com/Account/Login.cshtml?ReturnUrl=/Page/BookTicketPage.cshtml";
+        public static final String USERNAME="sonlexautrai@gmail.com";
+        public static final String PASSWORD ="Congson24012004.";
+
+    }
---
+++ scr/main/java/common/Utilities.java
@@ -0,0 +1,7 @@
+package common;
+
+import java.io.File;
+import java.util.Random;
+
+public class Utilities {}
+
